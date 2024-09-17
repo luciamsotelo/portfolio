5f88bd1503be65a6836ead88aa3e3d580e531f91
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-09-17 10:44:14 -0700

    contact page css responsive corrected

--- src/components/contact.js
+++ src/components/contact.js
@@ -82,41 +82,18 @@ function ContactPage() {
   };
 
   const getMessageClass = (index) => {
-    // Return a class based on whether the message is the current one
     return index === currentMessageIndex ? 'fade-in' : 'fade-out';
   };
 
   return (
-    <Container
-      className="contact-container my-5"
-      style={{
-        border: "10px solid #1c718e",
-        borderRadius: "10px",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-        backgroundColor: "rgb(234, 221, 207)",
-        position: "relative",
-      }}
-    >
-      <h1
-        className="text-center contact-title"
-        style={{
-          fontFamily: "ysabeau, cursive",
-          fontSize: "4rem",
-          textShadow: "2px 2px white",
-          position: "relative",
-          top: "10px",
-          transform: "translateX(-50%)",
-          zIndex: 10,
-          width: "100%",
-          textAlign: "center",
-        }}
-      >
+    <Container className="contact-container my-5">
+      <h1 className="text-center contact-title">
         Let's Collaborate!
       </h1>
 
-      <Row className="justify-content-center" style={{ marginTop: "5px"}}>
+      <Row className="justify-content-center" >
         <Col xs={12} md={6}>
-          <Form className="contact-form" style={{border: "6px inset hotpink", borderRadius: "1px"}} onSubmit={handleSubmit}>
+          <Form className="contact-form" style={{ background: "#e5d3bb" }} onSubmit={handleSubmit}>
             <Form.Group controlId="formName">
               <Form.Label>Your Name</Form.Label>
               <Form.Control
@@ -155,7 +132,6 @@ function ContactPage() {
               variant="primary"
               type="submit"
               className="w-100 contact-button"
-              style={{ marginTop: "20px" }}
             >
               Let's Build Together
             </Button>
@@ -163,58 +139,32 @@ function ContactPage() {
           {status && <p className="text-center mt-3">{status}</p>}
         </Col>
 
-        {/* Contact Information Section */}
-        <Col xs={12} md={6} className="my-5">
-          <Row className="g-3">
-            {/* Email */}
+        <Col xs={12} md={6} className="contact-info">
+          <Row className="g-3 mt-3">
             <Col xs={12}>
-              <Button
-                variant="outline-secondary"
-                href="mailto:luciamsotelo@yahoo.com"
-                className="w-100"
-                style={{ marginTop: "-10px", marginBottom: "1px" }}
-              >
+              <Button variant="outline-secondary" href="mailto:luciamsotelo@yahoo.com" className="w-100">
                 <FaEnvelope className="me-2" /> Email: luciamsotelo@yahoo.com
               </Button>
             </Col>
-            {/* Phone Number */}
             <Col xs={12}>
-              <Button
-                variant="outline-success"
-                href="tel:+1234567890"
-                className="w-100"
-                style={{ marginBottom: "1px" }} 
-              >
+              <Button variant="outline-success" href="tel:+1234567890" className="w-100">
                 <FaPhone className="me-2" /> Phone: (619) 343-3533
               </Button>
             </Col>
-            {/* GitHub */}
             <Col xs={12}>
-              <Button
-                variant="outline-dark"
-                href="https://github.com/luciamsotelo"
-                target="_blank"
-                className="w-100"
-                style={{ marginBottom: "1px" }} 
-              >
+              <Button variant="outline-dark" href="https://github.com/luciamsotelo" target="_blank" className="w-100">
                 <FaGithub className="me-2" /> GitHub: luciamsotelo
               </Button>
             </Col>
-            {/* LinkedIn */}
             <Col xs={12}>
-              <Button
-                variant="outline-primary"
-                href="https://www.linkedin.com/in/lucia-sotelo/"
-                target="_blank"
-                className="w-100"
-                style={{ marginBottom: "1px" }} 
-              >
+              <Button variant="outline-primary" href="https://www.linkedin.com/in/lucia-sotelo/" target="_blank" className="w-100">
                 <FaLinkedin className="me-2" /> LinkedIn: lucia-sotelo
               </Button>
             </Col>
-            {/* Random Messages Section */}
-            <Col xs={12} className="my-4" style={{}}>
-              <div className="messages" style={{ textAlign: "center", marginTop: "40px", fontFamily: "allura, cursive", color: "hotpink", textShadow: "1px 1px white" }}>
+          </Row>
+          <Row>
+            <Col xs={12} className="my-4">
+              <div className="messages">
                 {messages.map((message, index) => (
                   <p key={index} className={`message ${getMessageClass(index)}`}>
                     {message}
@@ -224,13 +174,14 @@ function ContactPage() {
             </Col>
           </Row>
         </Col>
-        <p className="text-center contact-subtitle" style={{paddingLeft: "50px", fontFamily: "ysabeau, cursive", fontSize: "1.5rem"}}>
-          I’d love to hear from you! Whether you’re part of a company looking
-          for a skilled developer or an individual seeking web design services,
-          feel free to reach out. Drop me a message, and I’ll get back to you as
-          soon as possible!
-        </p>
       </Row>
+
+      <p className="text-center" style={{ color: "#1c718e"}}>
+        I’d love to hear from you! Whether you’re part of a company looking
+        for a skilled developer or an individual seeking web design services,
+        feel free to reach out. Drop me a message, and I’ll get back to you as
+        soon as possible!
+      </p>
     </Container>
   );
 }
